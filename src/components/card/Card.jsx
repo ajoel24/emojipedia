67003c3273f3a2d55a11d1cfc9278b02326f046c
Andrew2432 <drew.devops@gmail.com>
2020-04-17 08:50:23 +0530

    Created Card component

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Card() {
+	return (
+		<section className="card">
+			<span></span>
+			<h3>Emoji Title</h3>
+			<p>Emoji Description</p>
+		</section>
+	);
+}
+
+export default Card;
