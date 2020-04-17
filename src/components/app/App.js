e179a5f6f5e5c9d2e118271875016f3c81fda0e7
Andrew2432 <drew.devops@gmail.com>
2020-04-17 09:04:45 +0530

    Added Card component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
+import Card from "../card/Card";
 
 function App() {
 	return (
 		<div>
 			<Header />
+			<Card />
 			<Footer />
 		</div>
 	);
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import "./Card.css";
 
 function Card() {
 	return (
 		<section className="card">
-			<span>🙂</span>
+			<span className="emoji">🙂</span>
 			<h3>Emoji Title</h3>
-			<p>Emoji Description</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam nesciunt
+				accusantium error laboriosam quibusdam harum dolore quia illo
+				consectetur. Ipsa doloremque maiores omnis, eligendi minima corporis qui
+				architecto? Vitae, in!
+			</p>
 		</section>
 	);
 }
