0dbb7b101e7d1bd9c69e3b1af791cea721887bb3
Andrew2432 <drew.devops@gmail.com>
2020-04-17 08:52:11 +0530

    Created styling for Card component

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Card() {
 	return (
 		<section className="card">
-			<span></span>
+			<span>🙂</span>
 			<h3>Emoji Title</h3>
 			<p>Emoji Description</p>
 		</section>
