9681d2514e857f08acac96d2e5feb7c349980ac8
Andrew2432 <drew.devops@gmail.com>
2020-04-17 09:07:03 +0530

    Created Cards component

---
+++ src/components/cards/Cards.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "../card/Card";
+
+function Cards() {
+	return (
+		<main>
+			<Card />
+			<Card />
+			<Card />
+		</main>
+	);
+}
+
+export default Cards;
