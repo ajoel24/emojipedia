70928a9ac517b97b62f7dd3b218a065c12117e26
Andrew2432 <drew.devops@gmail.com>
2020-04-17 08:40:57 +0530

    Created Footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<footer>
+			<h3>&copy; {new Date().getFullYear()}</h3>
+			<h3>Made by Andrew</h3>
+		</footer>
+	);
+}
+
+export default Footer;
